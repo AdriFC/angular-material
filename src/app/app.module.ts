8399db84e9ece5a8f434825032563c02e834d2f3
AdriFC <adri86vigo@gmail.com>
2021-05-17 16:17:07 +0200

    prácticaMaterial&API-ANGULAR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSliderModule } from '@angular/material/slider';
+import { MaterialModule } from './material.module';
+import { PhotosComponent } from './components/photos/photos.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MaterialModule,
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
